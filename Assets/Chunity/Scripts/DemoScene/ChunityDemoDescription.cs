ca63f6f7156bb3f8b91187b156863772c4fe42d0
Nestor Napoles Lopez <napulen@gmail.com>
2019-06-28 09:40:40 -0400

    Revert "New: Importing Chunity to the template project"
    This reverts commit 07209d169126961ec986e61a597cf0d396e4690c.

--- Assets/Chunity/Scripts/DemoScene/ChunityDemoDescription.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ChunityDemoDescription : MonoBehaviour
-{
-    [TextArea]
-    public string description;
-}
