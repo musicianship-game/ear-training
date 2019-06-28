56eaab1cd3587ee36002e014006c86c9e850c9db
Nestor Napoles Lopez <napulen@gmail.com>
2019-06-28 09:28:51 -0400

    Adding Chunity asset and simple working example

---
+++ Assets/Scripts/ChunityAudioSourceController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunityAudioSourceController : MonoBehaviour {
+    void Start()
+    {        
+        GetComponent<ChuckSubInstance>().RunCode(@"
+		SinOsc foo => dac;
+		while( true )
+		{
+			Math.random2f( 300, 1000 ) => foo.freq;
+			100::ms => now;
+		}
+	");
+    }
+}
