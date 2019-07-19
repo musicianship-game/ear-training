ea4372b44f07d723b24e5968d385ce918a7da217
Nestor Napoles Lopez <napulen@gmail.com>
2019-07-19 14:19:11 -0400

    Adding the goal zone logic to the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,17 +10,23 @@ public class PlayerController : MonoBehaviour {
     private float vertical;
     private bool jumping;
     private bool falling;
+    private bool isAtGoalZone;
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
         spriteanimator = transform.Find("PlayerSprite").gameObject.GetComponent<Animator>();
         speed = 5f;
+        isAtGoalZone = false;
     }
 	
 	void Update()
     {
         Vector2 translation;
+        if (isAtGoalZone)
+        {
+            Debug.Log("At Goal Zone");
+        }
         if (jumping)
         {
             translation = new Vector2(0f, 1f) * speed * 2f;
@@ -62,7 +68,11 @@ public class PlayerController : MonoBehaviour {
                 jumping = false;
                 falling = true;
             }                       
-        }        
+        }
+        else if (collision.gameObject.tag == "GoalZone")
+        {
+            isAtGoalZone = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -75,6 +85,10 @@ public class PlayerController : MonoBehaviour {
                 jumping = false;
                 falling = false;
             }
+        }
+        else if (collision.gameObject.tag == "GoalZone")
+        {
+            isAtGoalZone = false;
         }
     }
 }
