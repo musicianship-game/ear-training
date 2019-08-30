5ae046c40beadc0450b082b7a8852300599bed07
Nestor Napoles Lopez <napulen@gmail.com>
2019-08-30 14:14:50 -0400

    Adding basic logic for loading the main menu and loading the first scene from there

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class UIController : MonoBehaviour {
+    public Button playButton;    
+    public Button quitButton;
+
+    void Start()
+    {
+        //Calls the TaskOnClick/TaskWithParameters/ButtonClicked method when you click the Button
+        playButton.onClick.AddListener(Play);
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
