fc3e76f3d137b38cf27020e3ee8a7a4e1a8c465f
Nestor Napoles Lopez <napulen@gmail.com>
2019-08-31 13:58:55 -0400

    Adding the user settings UI for selecting the notation and scale

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+using System.IO;
+
+
 
 public class UIController : MonoBehaviour {
-    public Button playButton;    
+    public Button playButton;
     public Button quitButton;
+    public Button settingsButton;
+    public UIScaleSelector settingsPanel;
 
     void Start()
     {
         //Calls the TaskOnClick/TaskWithParameters/ButtonClicked method when you click the Button
         playButton.onClick.AddListener(Play);
         quitButton.onClick.AddListener(Quit);
+        settingsButton.onClick.AddListener(LoadSettings);
     }
 
     void Play()
@@ -24,5 +30,11 @@ public class UIController : MonoBehaviour {
     void Quit()
     {
         Application.Quit();
+    }
+
+    void LoadSettings()
+    {
+        settingsPanel.gameObject.SetActive(true);
+        settingsPanel.Activate();
     }
 }
---
+++ Assets/Scripts/UIScaleSelector.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using System.IO;
+
+public class UIScaleSelector : MonoBehaviour {
+	private Text notationText;
+	private Dropdown notationDropdown;
+	// private Text scaleText;
+	private Dropdown scaleDropdown;
+	private Button acceptButton;
+	private Button cancelButton;
+	private string notationsDir;
+
+	private DirectoryInfo[] notationDirInfos;
+	private DirectoryInfo[] scaleDirInfos;
+
+	private void Awake() {
+		notationsDir = @"Assets/Scales";
+		notationText = transform.Find("NotationsText").GetComponent<Text>();
+		notationDropdown = transform.Find("NotationsDropdown").GetComponent<Dropdown>();
+		notationDropdown.ClearOptions();
+		// scaleText = transform.Find("ScalesText").GetComponent<Text>();
+		scaleDropdown = transform.Find("ScalesDropdown").GetComponent<Dropdown>();
+		scaleDropdown.interactable = false;
+		scaleDropdown.ClearOptions();
+		acceptButton = transform.Find("AcceptButton").GetComponent<Button>();
+		acceptButton.interactable = false;
+		cancelButton = transform.Find("CancelButton").GetComponent<Button>();
+		notationDropdown.onValueChanged.AddListener(NotationChanged);
+		scaleDropdown.onValueChanged.AddListener(ScaleChanged);
+		acceptButton.onClick.AddListener(AcceptChanges);
+		cancelButton.onClick.AddListener(CancelChanges);
+	}
+
+	public void Activate() {
+		DirectoryInfo root = new DirectoryInfo(notationsDir);
+        notationDirInfos = root.GetDirectories();
+        List<string> notationNames = new List<string>();
+		notationNames.Add("None");
+		foreach (DirectoryInfo dir in notationDirInfos)
+        {
+			notationNames.Add(dir.Name);
+        }
+		notationDropdown.ClearOptions();
+		notationDropdown.AddOptions(notationNames);
+	}
+
+	public void NotationChanged(int value) {
+		scaleDropdown.ClearOptions();
+		if (value == 0)
+		{
+			scaleDropdown.interactable = false;
+			acceptButton.interactable = false;
+		}
+		else
+		{
+			scaleDropdown.interactable = true;
+			DirectoryInfo notationRoot = notationDirInfos[value - 1];
+			scaleDirInfos = notationRoot.GetDirectories();
+			List<string> scaleNames = new List<string>();
+			scaleNames.Add("None");
+			foreach (DirectoryInfo dir in scaleDirInfos)
+        	{
+				scaleNames.Add(dir.Name);
+        	}
+			scaleDropdown.AddOptions(scaleNames);
+			scaleDropdown.value = 0;
+		}
+	}
+
+	public void ScaleChanged(int value) {
+		if (value == 0)
+		{
+			acceptButton.interactable = false;
+		}
+		else
+		{
+			acceptButton.interactable = true;
+		}
+	}
+
+	public void AcceptChanges() {
+		// Read the CSV
+		List<string> NoteNames = new List<string>();
+		List<string> Frequencies = new List<string>();
+		string scaleDir = scaleDirInfos[scaleDropdown.value - 1].FullName;
+		string csvPath = Path.Combine(scaleDir, @"values.csv");
+		using(StreamReader reader = new StreamReader(csvPath))
+    	{
+			int lineType = 0;
+			while (!reader.EndOfStream)
+			{
+				var line = reader.ReadLine();
+				var values = line.Split(',');
+				foreach (string value in values)
+				{
+					bool isFrequency = lineType % 2 == 0 ? false : true;
+					if (isFrequency) {
+						Frequencies.Add(value);
+					}
+					else{
+						NoteNames.Add(value);
+					}
+				}
+			}
+    	}
+		foreach (string note in NoteNames)
+		{
+			Debug.Log(note);
+		}
+		foreach (string frequency in Frequencies)
+		{
+			Debug.Log(frequency);
+		}
+	}
+
+	public void CancelChanges() {
+		notationDropdown.value = 0;
+		scaleDropdown.value = 0;
+		notationDropdown.ClearOptions();
+		scaleDropdown.ClearOptions();
+		gameObject.SetActive(false);
+	}
+}
