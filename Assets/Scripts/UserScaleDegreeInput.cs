a7094f26e81d0cb204b99677f605ba80e3354150
Nestor Napoles Lopez <napulen@gmail.com>
2019-09-19 11:01:22 -0400

    Adding Alteration User Input Buttons

--- Assets/Scripts/UserScaleDegreeInput.cs
+++ Assets/Scripts/UserScaleDegreeInput.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UserScaleDegreeInput : MonoBehaviour {
+	private int alterationUp;
+	private int alterationDown;
+	private int alterationMultiplier;
 
-	// Use this for initialization
-	void Start () {
-		
+	public int alteration;
+
+	private void Start()
+	{
+		alterationUp = 0;
+		alterationDown = 0;
+		alterationMultiplier = 1;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+	private void Update()
+    {
+		// Get the inputs
+        if (Input.GetButtonDown("AlterationUp"))
+		{
+			Debug.Log("Oh oh oh!");
+			alterationUp = 1;
+		}
+		else if (Input.GetButtonUp("AlterationUp")) alterationUp = 0;
+
+		if (Input.GetButtonDown("AlterationDown")) alterationDown = 1;
+		else if (Input.GetButtonUp("AlterationDown")) alterationDown = 0;
+
+		if (Input.GetButtonDown("AlterationMultiplier")) alterationMultiplier = 2;
+		else if (Input.GetButtonUp("AlterationMultiplier")) alterationMultiplier = 1;
+
+		// Update the current alteration value
+		alteration = (alterationUp - alterationDown) * alterationMultiplier;
+    }
 }
