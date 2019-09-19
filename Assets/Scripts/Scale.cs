faccedbd35c3f8337bb6e08a3367e2b8eed38cc9
Nestor Napoles Lopez <napulen@gmail.com>
2019-09-19 11:57:50 -0400

    Linking up user input with singing!

--- Assets/Scripts/Scale.cs
+++ Assets/Scripts/Scale.cs
@@ -12,25 +12,37 @@ public static class Scale {
     {
         NoteNames = new List<string>()
         {
-            "C", "D", "E", "F", "G", "A", "B"
+            "C", "D", "E", "F", "G", "A", "B",
+            "C#", "D#", "E#", "F#", "G#", "A#", "B#",
+            "Cx", "Dx", "Ex", "Fx", "Gx", "Ax", "Bx",
+            "Cbb", "Dbb", "Ebb", "Fbb", "Gbb", "Abb", "Bbb",
+            "Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb"
         };
         Frequencies = new List<float>()
         {
-            261.63f, 293.66f, 329.63f, 349.23f, 392.00f, 440.00f, 493.88f
+            261.63f, 293.66f, 329.63f, 349.23f, 392.00f, 440.00f, 493.88f,
+            277.18f, 311.13f, 349.23f, 369.99f, 415.30f, 466.16f, 523.25f,
+            293.66f, 329.63f, 369.99f, 392.00f, 440.00f, 493.88f, 554.37f,
+            233.08f, 261.63f, 293.66f, 311.13f, 349.23f, 392.00f, 440.00f,
+            246.94f, 277.18f, 311.13f, 329.63f, 369.99f, 415.30f, 466.16f
         };
-        ScaleDegrees = NoteNames.Count;
-        Alterations = 1;
+        ScaleDegrees = 7;
+        Alterations = 5;
+    }
+
+    private static int GetNoteIndex(int scaleDegree, int alteration)
+    {
+        alteration = alteration >= 0 ? alteration : alteration + Alterations;
+        return alteration * ScaleDegrees + scaleDegree;
     }
 
     public static string GetNoteName(int scaleDegree, int alteration = 0)
     {
-        int index = alteration * ScaleDegrees + scaleDegree;
-        return NoteNames[index];
+        return NoteNames[GetNoteIndex(scaleDegree, alteration)];
     }
 
     public static float GetNoteFrequency(int scaleDegree, int alteration = 0)
     {
-        int index = alteration * ScaleDegrees + scaleDegree;
-        return Frequencies[index];
+        return Frequencies[GetNoteIndex(scaleDegree, alteration)];
     }
 }
--- Assets/Scripts/UserScaleDegreeInput.cs
+++ Assets/Scripts/UserScaleDegreeInput.cs
@@ -7,23 +7,23 @@ public class UserScaleDegreeInput : MonoBehaviour {
 	private int alterationDown;
 	private int alterationMultiplier;
 
-	public int alteration;
+	private int alteration;
+	private int scaleDegree;
+
+	private PlayerController playerController;
 
 	private void Start()
 	{
 		alterationUp = 0;
 		alterationDown = 0;
 		alterationMultiplier = 1;
+		playerController = GetComponent<PlayerController>();
 	}
 
 	private void Update()
     {
 		// Get the inputs
-        if (Input.GetButtonDown("AlterationUp"))
-		{
-			Debug.Log("Oh oh oh!");
-			alterationUp = 1;
-		}
+        if (Input.GetButtonDown("AlterationUp")) alterationUp = 1;
 		else if (Input.GetButtonUp("AlterationUp")) alterationUp = 0;
 
 		if (Input.GetButtonDown("AlterationDown")) alterationDown = 1;
@@ -34,5 +34,29 @@ public class UserScaleDegreeInput : MonoBehaviour {
 
 		// Update the current alteration value
 		alteration = (alterationUp - alterationDown) * alterationMultiplier;
+
+		// Handle any spelling of notes from the user
+		if (Input.GetButtonDown("ScaleDegree1")) SpellScaleDegre(1);
+		if (Input.GetButtonDown("ScaleDegree2")) SpellScaleDegre(2);
+		if (Input.GetButtonDown("ScaleDegree3")) SpellScaleDegre(3);
+		if (Input.GetButtonDown("ScaleDegree4")) SpellScaleDegre(4);
+		if (Input.GetButtonDown("ScaleDegree5")) SpellScaleDegre(5);
+		if (Input.GetButtonDown("ScaleDegree6")) SpellScaleDegre(6);
+		if (Input.GetButtonDown("ScaleDegree7")) SpellScaleDegre(7);
+		if (Input.GetButtonDown("ScaleDegree8")) SpellScaleDegre(8);
+		if (Input.GetButtonDown("ScaleDegree9")) SpellScaleDegre(9);
+		if (Input.GetButtonDown("ScaleDegree10")) SpellScaleDegre(10);
     }
+
+	private void SpellScaleDegre(int n)
+	{
+		if (n >= 1 && n <= Scale.ScaleDegrees)
+		{
+			scaleDegree = n - 1;
+			string noteName = Scale.GetNoteName(scaleDegree, alteration);
+			float freq = Scale.GetNoteFrequency(scaleDegree, alteration);
+			Debug.Log(noteName + " (" + freq + "Hz)");
+			playerController.Sing(noteName, freq);
+		}
+	}
 }
