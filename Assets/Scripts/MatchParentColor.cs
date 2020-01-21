d5bd538a5145da23640c4ce85dba1ba40e8bcdd1
Bazzal <Bazzal@Bazzals-MacBook-Air.local>
2020-01-21 10:42:42 -0500

    made the gate have a front and back side so things can pass through

---
+++ Assets/Scripts/MatchParentColor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchParentColor : MonoBehaviour {
+
+    public Color glow_color;
+
+    // Use this for initialization
+    void Start () {
+        glow_color = GetComponent<SpriteRenderer>().color;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        GetComponent<SpriteRenderer>().color = glow_color;
+	}
+}
