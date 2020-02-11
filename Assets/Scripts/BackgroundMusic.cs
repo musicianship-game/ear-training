632941d3427fc889904c034ea635efbee7974458
Nestor Napoles Lopez <napulen@gmail.com>
2020-02-11 12:06:29 -0500

    Adding background music

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class BackgroundMusic {
+    public static int pointer;
+    public static int[,] music;
+
+    static BackgroundMusic()
+    {
+        pointer = 0;
+        music = new int[,] {
+            {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 7, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+    }
+
+    public static int[,] GetNextEvent()
+    {
+        int[,] ev = new int[,] {
+            { music[0, pointer], music[1, pointer] },
+            { music[2, pointer], music[3, pointer] },
+            { music[4, pointer], music[5, pointer] },
+            { music[6, pointer], music[7, pointer] }
+        };
+        pointer = (pointer + 1) % music.GetLength(1);
+        return ev;
+    }
+}
---
+++ Assets/Scripts/BackgroundMusicPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicPlayer : MonoBehaviour {
+	public int bpm;
+	private float eventDelta;
+	private float t;
+	private ChuckSubInstance chuck;
+
+	// Use this for initialization
+	void Start ()
+	{
+		// If bpm not set, set it to 120
+		chuck = GetComponent<ChuckSubInstance>();
+		bpm = bpm == 0 ? 120 : bpm;
+		SetBPM(bpm);
+		t = eventDelta;
+	}
+
+	void SetBPM(int newBpm)
+	{
+		bpm = newBpm;
+		int quartersPerSecond = bpm / 60;
+		int sixteenthsPerSecond = quartersPerSecond * 4;
+		eventDelta = 1f / sixteenthsPerSecond;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		t -= Time.fixedDeltaTime;
+		if (t <= 0f)
+		{
+			int [,] notes = BackgroundMusic.GetNextEvent();
+			// TODO: Play the synths
+			int degree1 = notes[0, 0] - 1;
+			int alteration1 = notes[0, 1];
+			if (degree1 >= 0)
+			{
+				float frequency1 = Scale.GetNoteFrequency(degree1, alteration1) / 2f;
+				chuck.RunCode(ChuckSynths.BG_Plucked_String(frequency1));
+			}
+			// Debug.Log(notes);
+			t = eventDelta;
+		}
+	}
+}
