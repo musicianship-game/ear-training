6f19e5db71d5cdb78d2aff93af1255331ddf4ac0
Néstor Nápoles <napulen@gmail.com>
2020-05-20 11:02:32 -0400

    Time elapsed
    Time elapsed added to Game Over stats

--- Assets/Scripts/UIGameOver.cs
+++ Assets/Scripts/UIGameOver.cs
@@ -5,16 +5,24 @@ using UnityEngine.UI;
 using TMPro;
 
 public class UIGameOver : MonoBehaviour {
-    //public TextMeshProUGUI timeElapsed;
+    public TextMeshProUGUI timeElapsed;
     public TextMeshProUGUI scale;
     public TextMeshProUGUI score;
     public TextMeshProUGUI defeated;
     public TextMeshProUGUI accuracy;
     public TextMeshProUGUI misspellings;
 
+    string GetTimeDurationString()
+    {
+        float duration = Time.time - PlayerCloud.menu_time;
+        int minutes = Mathf.FloorToInt(duration / 60f);
+        int seconds = (int)duration % 60;
+        return "" + minutes + ":" + seconds;
+    }
+
 	// Use this for initialization
 	void Start () {
-        //timeElapsed = transform.Find("TimeElapsed").GetComponent<TextMeshProUGUI>();
+        timeElapsed = transform.Find("TimeElapsedText").GetComponent<TextMeshProUGUI>();
         scale = transform.Find("ScaleText").GetComponent<TextMeshProUGUI>();
         score = transform.Find("ScoreText").GetComponent<TextMeshProUGUI>();
         defeated = transform.Find("EnemiesDefeatedText").GetComponent<TextMeshProUGUI>();
@@ -22,7 +30,7 @@ public class UIGameOver : MonoBehaviour {
         misspellings = transform.Find("MisspelledNotesText").GetComponent<TextMeshProUGUI>();
 
         scale.text = Scale.Name;
-        //timeElapsed.text = "Duration of the game: " + (Time.time - PlayerCloud.menu_time);
+        timeElapsed.text = "Duration of the game: " + GetTimeDurationString();
         score.text = "Score: " + PlayerCloud.score;
         defeated.text = "Defeated enemies: " + PlayerCloud.enemies_defeated;
         accuracy.text = "Accuracy: " + Mathf.Round(PlayerCloud.GetAccuracy() * 100f) + "%";
