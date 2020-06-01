73ee0c27ad2f0395be41ac19c418baa3ab3c2d09
Néstor Nápoles <napulen@gmail.com>
2020-06-01 09:12:07 -0400

    Ignoring bell face object in resonate function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -183,6 +183,7 @@ public class PlayerController : MonoBehaviour {
             PlayerCloud.shots_fired++;
             foreach(Transform child in spawner.transform)
             {
+                if (child.name == "boss_face") continue;
                 if (child.GetComponent<ThisTarget>().Resonate(note_freq))
                 {
                     enemies_shot++;
