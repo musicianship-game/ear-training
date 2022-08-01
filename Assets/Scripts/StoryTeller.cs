7f59953205c9d29422bbc402e5fbceba51b1670a
Néstor Nápoles López <napulen@gmail.com>
2022-08-01 13:14:27 -0400

    Add Queue for SceneText on intro scene

--- Assets/Scripts/StoryTeller.cs
+++ Assets/Scripts/StoryTeller.cs
@@ -8,17 +8,50 @@ public class StoryTeller : MonoBehaviour {
 
     public TextMeshProUGUI title_text;
     private float title_fade_end;
+    private float text_start_time;
+    private float text_fade_end;
     public float title_fade_duration = 3.0f;
+    private float text_duration;
     private Color title_color;
     private bool title_fade = false;
+    private bool text_fade = false;
     public TextMeshProUGUI scene_text;
     private Color scene_text_color;
-    private float scene_text_end_time = 0.0f;
     private float start_time;
     public SpriteRenderer sunset_pic;
     public float sky_fade_time = 20.0f;
     private float sky_end_time;
     private Color sky_color;
+    private SceneText current_scene_text = null;
+    private Queue<SceneText> texts;
+
+    public class SceneText
+    {
+        public float start_time;
+        public float end_time;
+        public float duration;
+        public string text;
+        public SceneText(float start, float end, string t)
+        {
+            start_time = start;
+            end_time = end;
+            duration = end_time - start_time;
+            text = t;
+        }
+        public bool IsActive(float current_time)
+        {
+            return true ? HasStarted(current_time) && !HasEnded(current_time) : false;
+        }
+        public bool HasStarted(float current_time)
+        {
+            return true ? current_time >= start_time : false;
+        }
+
+        public bool HasEnded(float current_time)
+        {
+            return true ? current_time > end_time : false;
+        }
+    }
 
     // Use this for initialization
     void Start () {
@@ -34,8 +67,12 @@ public class StoryTeller : MonoBehaviour {
         title_color.a = 0.0f;
         title_text.color = title_color;
         FadeInTitle();
-        WriteSceneText("Once upon a time..", 5.0f);
-	}
+        texts = new Queue<SceneText>();
+        texts.Enqueue(new SceneText(1.0f, 5.0f, "When night falls..."));
+        texts.Enqueue(new SceneText(6.0f, 11.0f, "...ghosts begin to haunt the village."));
+        texts.Enqueue(new SceneText(12.0f, 16.0f, "They can be freed from this place..."));
+        texts.Enqueue(new SceneText(17.0f, 20.0f, "...through Music!"));
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -43,39 +80,77 @@ public class StoryTeller : MonoBehaviour {
         {
             SceneManager.LoadScene(1);
         }
-        if (Time.time > sky_end_time)
-        {
-            sky_color.a = 0.0f;
-            sunset_pic.color = sky_color;
-        }
-        else
+        // Sky stuff
+        if (Time.time < sky_end_time)
         {
             sky_color.a = (sky_end_time - Time.time) / sky_fade_time;
             sunset_pic.color = sky_color;
         }
-        if (Time.time > scene_text_end_time)
-        {
-            scene_text_color.a = 0.0f;
-            scene_text.color = scene_text_color;
+        else {
+            sky_color.a = 0.0f;
+            sunset_pic.color = sky_color;
         }
-        if (title_fade && Time.time < title_fade_end)
+        // Title stuff
+        if (title_fade)
         {
-            title_color.a = 1.0f - (title_fade_end - Time.time) / title_fade_duration;
-            title_text.color = title_color;
+            if (Time.time < title_fade_end)
+            { 
+                title_color.a = 1.0f - (title_fade_end - Time.time) / title_fade_duration;
+                title_text.color = title_color;
+            }
+            else
+            {
+                title_color.a = 1.0f;
+                title_text.color = title_color;
+                title_fade = false;
+            }
         }
-        else if (title_fade && Time.time >= title_fade_end)
+
+        if (texts.Count > 0 && texts.Peek().HasStarted(Time.time))
         {
-            title_color.a = 1.0f;
-            title_text.color = title_color;
+            current_scene_text = texts.Dequeue();
+            WriteSceneText(current_scene_text.text, current_scene_text.duration);
+        }
+        if (current_scene_text != null && !current_scene_text.IsActive(Time.time))
+        {
+            text_fade = false;
+            current_scene_text = null;
+            Debug.Log("Killing a text!");
+        }
+
+        // Other text stuff
+        if (text_fade)
+        {
+            if (Time.time < text_fade_end)
+            {
+                scene_text_color.a = TextFadeFunc();
+                scene_text.color = scene_text_color;
+            }
+            else
+            {
+                scene_text_color.a = 0.0f;
+                scene_text.color = scene_text_color;
+                text_fade = false;
+            }
         }
     }
 
+    public float TextFadeFunc()
+    {
+        float normalized_fade = (text_fade_end - Time.time) / text_duration;
+        float centered_fade = Mathf.Abs(normalized_fade - 0.5f);
+        float fade_envelope = Mathf.Min(((0.5f - centered_fade) * 10.0f), 1.0f);
+        return fade_envelope;
+    }
+    
     public void WriteSceneText(string text, float duration)
     {
+        text_fade = true;
         scene_text.text = text;
-        scene_text_color.a = 1.0f;
+        scene_text_color.a = 0.0f;
         scene_text.color = scene_text_color;
-        scene_text_end_time = Time.time + duration;
+        text_duration = duration;
+        text_fade_end = Time.time + duration;
     }
 
     public void FadeInTitle()
